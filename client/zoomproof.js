6bce481d212caf34d7990cb025724dd93cf5bdf5
mahnerak <mail@mahnerak.com>
2014-05-24 17:17:17 +0500

    Cleanig up

--- client/zoomproof.js
+++ client/zoomproof.js
@@ -1,66 +1,71 @@
-$.when(
-		//$.getScript("/w/index.php?title=User:HrantKhachatrian/morebits.js&action=raw&ctype=text/javascript"),
-		$.Deferred(function (deferred) {
-			$(deferred.resolve);
-		})
-).done(function () {
-	var pageName = mw.config.get('wgPageName');
+$(function () {
+	var pageName = mw.config.get("wgPageName");
 	
-	console.log(pageName);
 	if(!pageName.match(/Էջ:.*\.djvu\/\d+/)) return;
-	console.log(pageName);
 	
-	var jsonUrl = pageName.replace(/djvu/, "djvu.json").replace("Էջ:", "");
-	var json;
-	$.getJSON("/w/index.php?title=" + encodeURIComponent(jsonUrl) + "&action=raw&ctype=text/javascript", function (data) {
-		json = data;
-		var $editorTextarea = $('[name=wpTextbox1]');
-		var $editorDivContentEditable = $('<div contenteditable/>')
+	var fileName = pageName.replace("Էջ:", "");
+	var jsonFile = fileName.replace(/djvu/, "djvu.json");
+	var jsonUrl = "/w/index.php?title=%s&action=raw&ctype=text/javascript".replace("%s", jsonFile);
+	
+	$.getJSON(jsonUrl, function (json) {
+		var $editorTextarea = $('#wpTextbox1');
+		var editorHeight = $editorTextarea.height();
+		var editorWidth = $editorTextarea.width();
+		var $editorDivContentEditable = $('<div>')
+		.attr("contenteditable", "true")
 		.insertBefore($editorTextarea)
 		.css({
-			height: $editorTextarea.height(), 
-			width: $editorTextarea.width(), 
-			backgroundColor: '#fff', 
-			position:'relative', 
-			top: 0, 
-			left: 0, 
-			'margin-bottom': -$editorTextarea.height(),
-			zIndex: 100,
-			'overflow': 'scroll',
-			'overflow-x': 'hidden',
-			'overflow-y': 'scroll',
+			"height": 		editorHeight, 
+			"width": 		editorWidth, 
+			"background-color": 	"white", 
+			"position": 		"relative", 
+			"top": 			0, 
+			"left": 		0,
+			"margin-bottom": 	-editorHeight,
+			"z-index": 		100,
+			"overflow": 		"scroll',
+			"overflow-x": 		"hidden",
+			"overflow-y": 		"scroll"
 		});
+		
 		var hintWidth = 700;
 		var hintHeight = 130;
 		var transitionSpeed = 0.2;
-		var $hint = $("<div/>").css({
-			position: 'absolute',
-			'z-index': '500',
-			top: 100,
-			left: 100,
-			width: hintWidth,
-			height: hintHeight,
-			overflow: 'hidden',
-			'background-color': 'white',
-			border: '4px solid #ff0',
-			transition: 'all ' + transitionSpeed + 's ease-out'
-//			'border-top-left-radius': '300px',
-//			'border-top-right-radius': '300px'
+		
+		var $hint = $("<div>")
+		.css({
+			"position": 		"absolute",
+			"z-index": 		500,
+			"top": 			100,
+			"left": 		100,
+			"width": 		hintWidth,
+			"height":		hintHeight,
+			"overflow": 		"hidden",
+			"background-color": 	"white",
+			"border": 		"4px solid #ff0",
+			"transition": 		"all %ds ease-out".replace("%d", transitionSpeed)
 		}).insertAfter($editorDivContentEditable);
 	
-		var $hintImage = $("<img>").appendTo($hint).attr("src", $(".prp-page-image img").attr("src")).css({
-			'position': 'absolute',
-			transition: 'all ' + transitionSpeed + 's ease-out',
-			'margin-top': 100,
-			'margin-left': hintWidth / 2
+		var imageUrl = $(".prp-page-image img").attr("src");
+		
+		var $hintImage = $("<img>")
+		.appendTo($hint)
+		.attr("src", imageUrl)
+		.css({
+			"position": 		"absolute",
+			"transition": 		"all %ds ease-out".replace("%d", transitionSpeed),
+			"margin-top": 		100,
+			"margin-left": 		hintWidth / 2
 		});
 		
-		var $ov = $("<div/>").appendTo($hint).css({
-			'position': 'absolute',
-			transition: 'all ' + transitionSpeed + 's ease-out',
-			top: 100,
-			left: hintWidth / 2,
-			'background-color': 'rgba(255, 255, 0, .5)'
+		var $ov = $("<div>")
+		.appendTo($hint)
+		.css({
+			"position": "absolute",
+			"transition": "all %ds ease-out".replace("%d", transitionSpeed),
+			"top": 100,
+			"left": hintWidth / 2,
+			"background-color": "rgba(255, 255, 0, .5)"
 		});
 		
 		var imageSize = {
@@ -71,7 +76,7 @@ $.when(
 		var words = json.words;
 		var pageSize = json.size;
 		
-		for(var i = 0; i < words.length; ++i){
+		for(var i in words){
 			var word = words[i].contents.replace("&#10;", "<br>"); 
 			$('<span/>')
 				.appendTo($editorDivContentEditable)
@@ -105,11 +110,5 @@ $.when(
 			});
 		});
 	});
-
-	//var currentPage = new Morebits.wiki.page();
-
-	//currentPage.load(function () {
-	
-	//});
 
 });
